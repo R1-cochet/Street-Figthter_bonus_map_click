d17b58c411e0a8d7d0ff48bcd83e4cb19944ee7f
Daniel TG <danytg-95@hotmail.com>
2016-02-19 21:04:44 -0500

    script control con flechas asignadas pero sin animación

--- SF_CarBuster/Assets/Scripts/Ken/Controls.cs
+++ SF_CarBuster/Assets/Scripts/Ken/Controls.cs
@@ -3,13 +3,19 @@ using System.Collections;
 
 public class Controls : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
 	
-	}
-	
-	// Update is called once per frame
 	void Update () {
-	
+	if (Input.GetKeyDown (KeyCode.RightArrow))
+			print ("flecha derecha a sido precionada");
+
+	if(Input.GetKeyDown(KeyCode.UpArrow))
+			print("flecha arriba a sido precionada");
+
+	if(Input.GetKeyDown(KeyCode.DownArrow))
+			print("flecha abajo a sido precionada");
+
+    if(Input.GetKeyDown(KeyCode.LeftArrow))
+			print("flecha Izquierda a sido precionada");
+
 	}
 }
