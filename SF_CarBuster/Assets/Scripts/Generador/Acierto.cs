d6e4b216767e18054710d4b857d0265c80dc991b
Eder Camilo Muñoz <edercamilo08@hotmail.com>
2016-02-22 22:20:50 -0500

    Empezando a implementar generador (Imagenes no oficiales, a espera de las imagenes de puño y patada).

---
+++ SF_CarBuster/Assets/Scripts/Generador/Acierto.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Acierto : MonoBehaviour {
+
+	Collider2D other;
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.RightArrow)) {
+			print ("flecha derecha a sido precionada");
+			if (other!=null && other.tag == "Patada") {
+				print ("Acierto");
+				other = null;
+			} else {
+				print ("No Acierto");
+			}
+		}
+
+		if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+			print ("flecha Izquierda a sido precionada");
+			if (other!=null && other.tag == "Puño") {
+				print ("Acierto");
+				other = null;
+			} else {
+				print ("No Acierto");
+			}
+		}	
+	}
+
+	void OnTriggerEnter2D(Collider2D other1){
+		other = other1;
+	}
+}
