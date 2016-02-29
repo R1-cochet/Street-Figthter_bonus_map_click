68bec465f2deb0bd61a74a342cb54c983826c299
Eder Camilo Muñoz <edercamilo08@hotmail.com>
2016-02-28 20:31:43 -0500

    Asociación animaciones con teclas

--- SF_CarBuster/Assets/Scripts/Generador/Acierto.cs
+++ SF_CarBuster/Assets/Scripts/Generador/Acierto.cs
@@ -4,19 +4,28 @@ using System.Collections;
 public class Acierto : MonoBehaviour {
 
 	Collider2D other;
+	public GameObject ken;
 	void Start () {
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		ken.GetComponent<Animator> ().SetBool ("Kick",false);
+		ken.GetComponent<Animator> ().SetBool ("Punch",false);
+		ken.GetComponent<Animator> ().SetBool ("Hit",false);
+
+
+
 		if (Input.GetKeyDown (KeyCode.RightArrow)) {
 			print ("flecha derecha a sido precionada");
 			if (other!=null && other.tag == "Patada") {
 				print ("Acierto");
+				ken.GetComponent<Animator> ().SetBool ("Kick",true);
 				other = null;
 			} else {
 				print ("No Acierto");
+				ken.GetComponent<Animator> ().SetBool ("Hit",true);
 			}
 		}
 
@@ -24,9 +33,11 @@ public class Acierto : MonoBehaviour {
 			print ("flecha Izquierda a sido precionada");
 			if (other!=null && other.tag == "Puño") {
 				print ("Acierto");
+				ken.GetComponent<Animator> ().SetBool ("Punch",true);
 				other = null;
 			} else {
 				print ("No Acierto");
+				ken.GetComponent<Animator> ().SetBool ("Hit",true);
 			}
 		}	
 	}
